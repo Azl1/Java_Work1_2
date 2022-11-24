5fd3ea0c8078afb07bde174f1100c65221e6709a
Aziz <abdullaev12@bk.ru>
2022-11-24 22:39:53 +0300

    add Hello World

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("Hello Worl");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        int value1 = sc.nextInt();
+        System.out.println("Введите второе число: ");
+        int value2 = sc.nextInt();
+        System.out.println("Введите третье число: ");
+        int value3 = sc.nextInt();
+
+        int result  = sum(value1, value2, value3);
+        System.out.println("Сумма чисел = " + result);
+    }
+    public static int sum(int value1, int value2, int value3) {
+        return value1 + value2 + value3;
     }
 }
