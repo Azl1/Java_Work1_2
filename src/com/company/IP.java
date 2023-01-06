fdf8f4d89b897ae76c6970fc510bf658dd7aa554
Aziz <abdullaev12@bk.ru>
2023-01-06 13:12:58 +0300

    Zadacha_1

--- src/com/company/IP.java
+++ src/com/company/IP.java
@@ -3,77 +3,81 @@ package com.company;
 import java.util.Scanner;
 
 public class IP {
+
     public static void main(String[] args) {
+         int operation = 0;
+         int earnings = 0;    // доходы
+         int spendings = 0;   // расходы
+         int usn_r = 0;
+         int usn_d_r = 0;
         Scanner scanner = new Scanner(System.in);
+        String input = "";
 
         while (true) {
             System.out.println("Выберите операцию и введите её номер:\n" +
                     "1. Добавить новый доход\n" +
                     "2. Добавить новый расход\n" +
-                    "3. Выбрать ситему налогооблажения");
-            String input = scanner.nextLine();
+                    "3. Выбрать ситему налогооблажения\n" +
+                    "для завершения работы нажмите - end");
+            input = scanner.nextLine();
+
             if ("end".equals(input)) {
+                //1    profit(earnings);
+                System.out.println("Доходы ИП: " + earnings + " рублей");
+                System.out.println("Расходы ИП: " + spendings + " рублей");
+                System.out.println("УСН доходы: " + usn_r(earnings) + " рублей");
+                System.out.println("УСН доходы минус расходы: " + usn_d_r(earnings, spendings) + " рублей");
+                System.out.println("Экономия: " + (usn_d_r(earnings, spendings) - usn_r(earnings)) + " рублей");
                 break;
             }
-            int earnings = 0;    // доходы
-            int spendings = 0;   // расходы
-            int resultat = 0;
-            int operation = Integer.parseInt(input);
+
+            operation = Integer.parseInt(input);
             switch (operation)
             {
                 case 1:
                     System.out.println("Введите сумму дохода:");
-                    String moneyStr1 = scanner.nextLine(); // Не используйте тут nextInt (!)
-                    int money1 = Integer.parseInt(moneyStr1);
-                    earnings += money1;
-                   // resultat = earnings + earnings;
-                   // profit(earnings);
-                 //   System.out.println("Доходы ИП: " + earnings + earnings + "=" + profit(earnings) + " рублей");
+                    input = scanner.nextLine(); // Не используйте тут nextInt (!)
+                    earnings = earnings + Integer.parseInt(input);
                     break;
                 case 2:
                     System.out.println("Введите сумму расхода:");
-                    String moneyStr2 = scanner.nextLine(); // Не используйте тут nextInt (!)
-                    int money2 = Integer.parseInt(moneyStr2);
-                    spendings += money2;
-                    consumption(spendings);
+                    input = scanner.nextLine(); // Не используйте тут nextInt (!)
+                    spendings = spendings + Integer.parseInt(input);
                     break;
                 case 3:
                     System.out.println("Выбрать ситему налогооблажения");
-                    if ("".equals(input)) {
-                    //1    profit(earnings);
-                        System.out.println("Доходы ИП: " + earnings + " + " + earnings + " = " + profit(earnings) + "рублей \n" +
-                                "Расходы ИП: " +  spendings + "рублей\n" +
-                                "УСН доходы: 6% от " + resultat + "рублей = " + 60 + "рублей\n" );
-//            УСН доходы минус расходы: 15% от 1000-100 рублей = 135 рублей
-//            Экономия: 135 - 60 = 75 рублей");
-                        break;
+                    int usn_r1 = usn_r(earnings);
+                    int usn_d_r1 = usn_d_r(earnings, spendings);
+                    if ( usn_r1 < usn_d_r1) {
+                        System.out.println("Мы советуем вам УСН доходы");
+                        System.out.println("Ваш налог составит: " + usn_r1 + " рублей");
+                        System.out.println("Налог на другой системе: " + usn_d_r1 + " рублей");
+                        System.out.println("Экономия: " + (usn_d_r1 - usn_r1) + " рублей");
+                        System.out.println("");
                     }
-//                default:
-//                    System.out.println("Такой операции нет");
+                    if ( usn_d_r1 < usn_r1) {
+                        System.out.println("Мы советуем вам УСН доходы минус расходы: ");
+                        System.out.println("Ваш налог составит: " + usn_d_r1 + " рублей");
+                        System.out.println("Налог на другой системе: " + usn_r1 + " рублей");
+                        System.out.println("Экономия: " + (usn_r1 - usn_d_r1) + " рублей");
+                        System.out.println("");
+                    }
+                        break;
+
+                default:
+                    System.out.println("Такой операции нет");
             }
 
         }
         System.out.println("Программа завершена!");
     }
-    public static int profit(int earnings) {
-        int tax1 = earnings + earnings ;
-        if (tax1 >=0) {
-        //    System.out.println("Доходы ИП: " + earnings + earnings + "=" + tax1 + " рублей");
-            return tax1;
 
-        }
-            return 0;
-    }
-    public static int consumption(int spendings) {
-        int tax2 = (spendings) ;
-        if (tax2 >=0) {
-            return tax2;
-        }
-        return 0;
-    }
+      public static int usn_r(int a) {
+        return (a * 6) / 100;
+      }
 
-    public static int taxEarningsMinusSpendings(int earnings, int spendings) {
-        int tax = (earnings - spendings) * 15 / 100;
+    public static int usn_d_r(int a, int b) {
+        int tax = (a - b) * 15 / 100;
         if (tax >= 0) {
             return tax;
         } else {
@@ -81,6 +85,4 @@ public class IP {
             return 0;
         }
     }
-
-
 }
