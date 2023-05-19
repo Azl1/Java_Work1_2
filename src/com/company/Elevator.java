ad352ed5fa393216011d60ecc667581491c7e241
Aziz <abdullaev12@bk.ru>
2023-05-19 22:17:01 +0300

    Floor

--- src/com/company/Elevator.java
+++ src/com/company/Elevator.java
@@ -9,16 +9,20 @@ public class Elevator {
 
     public final int START_FLOOR = 0;
     public final int END_FLOOR = 25;
+    int waitDoorsInSeconds = 10;
+    int waitMoveInSeconds = 5;
+
     String enterNumberMsg = "Введите номер этажа от 1 до " + END_FLOOR + " (для завершения введите 0)";
 
-
-    public void floors(){
+    public void floors() {
+        int floor = 0;
+        int totalSeconds = 0;
+        int previousFloor = -1;
         System.out.println("Добро пожаловать в лифт");
         while (true) {
-            int floor = 0;
             System.out.println(enterNumberMsg);
-
             try {
+                previousFloor = floor;
                 floor = scanner.nextInt();
                 if (floor < START_FLOOR || floor > END_FLOOR) {
                     System.out.println("Некорректно, попробуйте, снова");
@@ -26,34 +30,30 @@ public class Elevator {
                 }
                 deque.add(floor);
                 if (floor == 0) {
+                    System.out.println("Время затраченное лифтом на маршрут =: " + totalSeconds + " с.");
                     break;
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Ошибка, таких этажей нет! ");
                 scanner.nextLine();
             }
-        }
+
             System.out.println();
             System.out.println("Лифт поехал ");
+//          int currentFloor = 0;
+//            while (!deque.isEmpty()) {
+//                Integer floor = deque.poll();
+//
+//
+//          Integer floors = deque.poll();
+            System.out.println("Этаж " + floor);
+            if (previousFloor != -1) {
+                totalSeconds += Math.abs(floor - previousFloor) * waitMoveInSeconds;
+                totalSeconds += waitDoorsInSeconds;
 
-        int waitDoorsInSeconds = 10;
-        int waitMoveInSeconds = 5;
-        int totalSeconds = 0;
-        int previousFloor = -1;
-    //    int currentFloor = 0;
-            while (!deque.isEmpty()) {
-                //Integer floor = deque.poll();
-
-
-                Integer floors = deque.poll();
-                System.out.println("Этаж " + floors);
-                if (previousFloor != -1) {
-                    totalSeconds += Math.abs(floors - previousFloor) * waitMoveInSeconds;
-                    totalSeconds += waitDoorsInSeconds;
-
-                }
-                System.out.println("Время затраченное лифтом на маршрут =: " + totalSeconds + " с.");
             }
+
+        }
 
     }
 
