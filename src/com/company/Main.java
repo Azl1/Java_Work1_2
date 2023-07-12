5be254d6ef9a8dd31672037729c149ca690e5b73
Aziz <abdullaev12@bk.ru>
2023-07-12 21:09:03 +0300

    Server

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,72 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
 
+    static TreeMap<Integer, User> userMap = new TreeMap<Integer, User>();
+    static TreeMap<Integer, Set<String>> usersIp = new TreeMap<Integer, Set<String>>();
+    static TreeMap<String, Integer> serverLogs = new TreeMap<String, Integer>();
+
     public static void main(String[] args) {
-        User user = new User();
-        TreeMap<id, User> treeMap = new TreeMap<id, User>();
+        File serverLog = new File("server.log");
+        File users = new File("users.db");
+
+        try {
+            Scanner scanner = new Scanner(users);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.contains("ip;id;fio;address")) {
+                    continue;
+                }
+                System.out.println(line);
+                String[] parse = line.split(";");
+                String ip = parse[0];
+                int id = Integer.parseInt(parse[1]);
+                String fio = parse[2];
+                String address = parse[3];
+                userMap.put(id, new User(id, fio, address));
+                Set<String> setIp = usersIp.get(id);
+                if (setIp == null) setIp = new TreeSet<>();
+                setIp.add(ip);
+                usersIp.put(id, setIp);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Scanner scanner = new Scanner(serverLog);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String ip = line.split(":")[0];
+                Integer count = serverLogs.get(ip);
+                if (count == null) count = 0;
+                count++;
+                serverLogs.put(ip, count);
+
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(serverLogs);
+    }
+
+    public static String max() {
+        int max = 0;
+        String key = "";
+        for (Map.Entry<String, Integer> i : serverLogs.entrySet())  {
+            if( max < i.getValue()) {
+                max = i.getValue();
+                key = i.getKey();
+            }
+        }
+        return key;
     }
 }
--- src/com/company/User.java
+++ src/com/company/User.java
@@ -7,31 +7,20 @@ import java.util.Scanner;
 
 public class User {
     int id;
-    String surname;
-    String name;
-    String patronymic;
+    String fio;
     String address;
-    String residence;
+
+    public User(int id, String fio, String address) {
+        this.id = id;
+        this.fio = fio;
+        this.address = address;
+    }
+
 
     public User() {
     }
 
-    public User(int id, String surname, String name, String patronymic, String address, String residence) {
-        this.id = id;
-        this.surname = surname;
-        this.name = name;
-        this.patronymic = patronymic;
-        this.address = address;
-        this.residence = residence;
-    }
 
-    public void scanner() throws FileNotFoundException {
-        File file = new File("1.txt");
-        Scanner scanner = new Scanner(file);
 
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            System.out.println(line); //вывод данных из файла (пример)
-        }
-    }
+
 }
